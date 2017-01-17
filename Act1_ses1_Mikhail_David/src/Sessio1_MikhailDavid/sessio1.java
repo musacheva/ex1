9d4d8ebc1e3bc9a4865bf2c72abefbbea5704755
David <David@192.168.36.163>
2017-01-17 13:52:38 +0100

    dhbfkdhb

--- Act1_ses1_Mikhail_David/src/Sessio1_MikhailDavid/sessio1.java
+++ Act1_ses1_Mikhail_David/src/Sessio1_MikhailDavid/sessio1.java
@@ -4,7 +4,7 @@ import Keyboard.*;
 
 public class sessio1 {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {//hoisfowifnq
 
 		int files = files();
 		int columnes = columnes();
