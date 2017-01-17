b6f611b0838ec9cd36a9997fab151b3c368af8c8
David <David@192.168.36.163>
2017-01-17 10:33:31 +0100

    Falta retocar algun detalle,pero por lo general ya funciona

--- Act1_ses1_Mikhail_David/src/Sessio1_MikhailDavid/sessio1.java
+++ Act1_ses1_Mikhail_David/src/Sessio1_MikhailDavid/sessio1.java
@@ -2,7 +2,7 @@ package Sessio1_MikhailDavid;
 
 import Keyboard.*;
 
-public class sessio1 {//comentari
+public class sessio1 {
 
 	public static void main(String args[]) {
 
@@ -13,11 +13,12 @@ public class sessio1 {//comentari
 
 		int valor = valor(files, columnes);
 		EncenAleatori(files, columnes, tauler, valor);
+		System.out.println("Encenem la posició: ");
 		RecorreTauler(tauler);
 
-		for (int i = 0; i < NTirades(); i++) {
+		for (int i = 1; i <= NTirades(); i++) {
 			System.out.println("Tirada " + i + ":");
-			tirades(files, columnes);
+			tirades(tauler,files, columnes);
 			RecorreTauler(tauler);
 		}
 
@@ -39,12 +40,11 @@ public class sessio1 {//comentari
 
 	public static int columnes() {
 
-		System.out.println("Indica les dimensions del taulell");
-		System.out.println("Entra un número de files>2 i <=12");
+		System.out.println("Entra un número de columnes >2 i <=12");
 		int columnes = Keyboard.readInt();
 
 		while (columnes <= 2 || columnes > 12) {
-			System.out.println("Incorrecte. Entra files files>2 i <=12");
+			System.out.println("Incorrecte. Entra numero columnes >2 i <=12");
 			columnes = Keyboard.readInt();
 		}
 		return columnes;
@@ -76,9 +76,11 @@ public class sessio1 {//comentari
 					filaAleatoria = (int) Math.round(Math.random() * (files - 1));
 					ColumnaAleatoria = (int) Math.round(Math.random() * (columnes - 1));
 				}
+				tauler[filaAleatoria][ColumnaAleatoria] = 1;
 			}
 			i++;
 		}
+		
 	}
 
 	public static void RecorreTauler(int tauler[][]) {
@@ -93,76 +95,130 @@ public class sessio1 {//comentari
 
 	public static int NTirades() {
 
-		System.out.println("Especifica el numero de tirades");
+		System.out.println("Especifica el numero máxim de tirades");
 		int tirades = Keyboard.readInt();
 		return tirades;
 	}
 
-	public static void tirades(int files, int columnes) {
+	public static void tirades(int [][] tauler,int files, int columnes) {
 
 		System.out.println("**************");
 		System.out.println("Indica les coordenades del botó a premer");
 		System.out.println("Indica la fila:");
 		System.out.println("**************");
-		System.out.println("Especifica un valor entre l'interval [0, " + (files - 1));
+		System.out.println("Especifica un valor entre l'interval [0, " + (files - 1)+"]");
 		int fila = Keyboard.readInt();
 
 		while (fila < 0 || fila > files - 1) {
-			System.out.print("Valor de la fila fora de l'interval.Ha d'estar entre [0, " + (files - 1));
+			System.out.print("Valor de la fila fora de l'interval.Ha d'estar entre [0, " + (files - 1)+"]");
 			System.out.println("Torna a introduir el valor de la fila");
 			fila = Keyboard.readInt();
 		}
 
 		System.out.println("Indica la columna:");
 		System.out.println("**************");
-		System.out.println("Especifica un valor entre l'interval [0, " + (columnes - 1));
+		System.out.println("Especifica un valor entre l'interval [0, " + (columnes - 1)+"]");
 		int columna = Keyboard.readInt();
 
 		while (columna < 0 || columna > columnes - 1) {
-			System.out.print("Valor de la columna fora de l'interval.Ha d'estar entre [0, " + (files - 1));
+			System.out.print("Valor de la columna fora de l'interval.Ha d'estar entre [0, " + (files - 1)+"]");
 			System.out.println("Torna a introduir el valor de la columna");
 			columna = Keyboard.readInt();
 		}
+		
+		CanviEstat(tauler,fila,columna);
 
 	}
 
 	private static void CanviEstat(int[][] tauler, int fila, int columna) {
 		
-		
 		if(columna==0 && fila==0){
+			CanviEstat2(tauler,0,0);//columna==0 fila==0
+			CanviEstat2(tauler,0,1);//columna==1 fila==0      
+			CanviEstat2(tauler,1,0);//columna==0 fila==1
+			CanviEstat2(tauler,tauler.length-1,0);//columna==0 fila==tauler.length-1
+			CanviEstat2(tauler,0,tauler[0].length-1);//columna==tauler[0].length-1	fila==0
+		}
+		else if(columna==tauler[0].length-1 && fila==0){
 			
-			//columna==1 fila==0      //columna=columna+1 fila=fila
-			//columna==0 fila==1
-			//columna==0 fila==tauler.length-1
-			//columna==tauler[0].length-1	fila==0
+			CanviEstat2(tauler,0,tauler[0].length-1);//columna==tauler[0].length-1 && fila==0
+			CanviEstat2(tauler,1,tauler[0].length-1);//columna==tauler[0].length-1 && fila==1
+			CanviEstat2(tauler,tauler.length-1,tauler[0].length-1);//columna==tauler[0].length-1 && fila==tauler.length-1
+			CanviEstat2(tauler,0,tauler[0].length-2);//columna==tauler[0].length-2 && fila==0
+			CanviEstat2(tauler,0,0);//columna==0 && fila==0
+		}
+		else if(columna==0 && fila==tauler.length-1){
+			
+			CanviEstat2(tauler,tauler.length-1,0);//columna==0 && fila==tauler.length-1
+			CanviEstat2(tauler,tauler.length-1,1);//columna==1 && fila==tauler.length-1
+			CanviEstat2(tauler,tauler.length-2,0);//columna==0 && fila==tauler.length-2
+			CanviEstat2(tauler,0,0);//columna==0 && fila==0	
+			CanviEstat2(tauler,tauler.length-1,tauler[0].length-1);//columna=tauler[0].length-1 && fila=tauler.length-1
+		}
+		
+		else if(columna==tauler[0].length-1 && fila==tauler.length-1){
+			
+			CanviEstat2(tauler,tauler.length-1,tauler[0].length-1);//columna==tauler[0].length-1 && fila==tauler.length-1
+			CanviEstat2(tauler,tauler.length-2,tauler[0].length-1);//columna==tauler[0].length-1 && fila==tauler.length-2
+			CanviEstat2(tauler,0,tauler[0].length-1);//columna==tauler[0].length-1 && fila==0
+			CanviEstat2(tauler,tauler.length-1,tauler[0].length-2);//columna==tauler[0].length-2 && fila==tauler.length-1
+			CanviEstat2(tauler,tauler.length-1,0);//columna==0 && fila==tauler.length-1
+		}
 	
-		}
-		if(columna==tauler[0].length-1 && fila==0){
-			
-			//columna==tauler[0].length-1 && fila==0
-			//columna==tauler[0].length-1 && fila==1
-			//columna==tauler[0].length-1 && fila==tauler.length-1
-			//columna==tauler[0].length-1 && fila==0
-			//columna==0 && fila==0
-		}
-		if(columna==0 && fila==tauler.length-1){
-			
-			//columna==0 && fila==tauler.length-1
-			//columna==1 && fila==tauler.length-1
-			//columna==0 && fila==tauler.length-2
-			//columna==0 && fila==0	
-			//columna=tauler[0].length-1 && fila=tauler.length
-		}
-		
-		if(columna==tauler[0].length-1 && fila==tauler.length-1){
-			
-			//columna==tauler[0].length && fila==0
-			//
-			//
-			
-		}
-		
+		else if(fila==0){
+				CanviEstat2(tauler,0,columna);//fila==0 && columna==columna que el me pasa
+				CanviEstat2(tauler,0,columna-1);//fila==0 && columna==comuna que el me psa -1
+				CanviEstat2(tauler,1,columna);//fila=1 && columna==columna que le me psa
+				CanviEstat2(tauler,tauler.length-1,columna);//fila=tauler.length-1 && columna==columna que el me pasa
+			}
+			else if(fila==tauler.length-1){
+				CanviEstat2(tauler,tauler.length-1,columna);//fila==tauler.length-1 && columna==columna que me pasa
+				CanviEstat2(tauler,tauler.length-1,columna-1);//fila=tauler.length-1 && columna=columna que me pasa -1
+				CanviEstat2(tauler,tauler.length-1,columna+1);//fila=tauler.length-1 && columna=columna que me pasa +1
+				CanviEstat2(tauler,tauler.length-2,columna);//fila=tauler.length-2 && columna==columna que me pasa
+				CanviEstat2(tauler,0,columna);//fila==0 && columna==columna que me pasa
+			}
 
+			else if(columna==0){
+						CanviEstat2(tauler,fila,0);//columna==0 && fila==fila que me pasa
+						CanviEstat2(tauler,fila+1,0);//columna==0 && fila=fila que me pasa +1
+						CanviEstat2(tauler,fila+1,0);//columna==0 && fila=fila que me pasa +1
+						CanviEstat2(tauler,fila,1);//columna==1 && fila=fila que me pasa
+						CanviEstat2(tauler,fila,tauler[0].length-1);//columna==tauler[0].length-1 && fila=fila que me pasa
+					}
+			else if(columna==tauler[0].length-1){
+						CanviEstat2(tauler,fila,tauler[0].length-1);//columna==tauler[0].length-1 && fila==fila que pasa 
+						CanviEstat2(tauler,fila+1,tauler[0].length-1);//columna==tauler[0].length-1 && fila==fila que pasa +1
+						CanviEstat2(tauler,fila-1,tauler[0].length-1);//columna==tauler[0].length-1 && fila==fila que pasa -1
+						CanviEstat2(tauler,fila,tauler[0].length-2);//columna==tauler[0].length-2 && fila==fila que pasa
+						CanviEstat2(tauler,fila,0);//columna==0 && fila==fila que pasa
+			}else{
+			
+				CanviEstat2(tauler,fila,columna);
+				CanviEstat2(tauler,fila,columna+1);
+				CanviEstat2(tauler,fila,columna-1);
+				CanviEstat2(tauler,fila+1,columna);
+				CanviEstat2(tauler,fila-1,columna);				
+		}
+	
 	}
+
+	private static void CanviEstat2(int[][] tauler, int fila, int columna){
+		
+		if(tauler[fila][columna]==1){
+			tauler[fila][columna]=0;
+		}else{
+			tauler[fila][columna]=1;
+		}	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
